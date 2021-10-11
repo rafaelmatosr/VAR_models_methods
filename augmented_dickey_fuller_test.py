e8532ad3e135d2d46f689dd2ca8147aee156e54a
Rafael Matos <71857270+rafaelmatosr@users.noreply.github.com>
2021-10-11 02:10:53 -0300

    Update augmented_dickey_fuller_test.py

--- augmented_dickey_fuller_test.py
+++ augmented_dickey_fuller_test.py
@@ -8,17 +8,18 @@ def adf_test(series, signif=0.05, name='', verbose=False):
     t = adfuller(series, autolag='AIC')
     result = {'test_statistic':round(t[0], 4), 'pvalue':round(t[1], 4), 'n_lags':round(t[2], 4), 'n_obs':t[3]}
     p_value = result['pvalue'] 
-    def adjust(val, length= 6): return str(val).ljust(length)
+    def aux(value, length= 6): 
+        return str(value).ljust(length)
 
-    # Print Summary
+    #Summarize
     print(f'    Augmented Dickey-Fuller test for "{name}"', "\n   ", '-'*47)
-    print(f' Null Hypothesis: Data has unit root (non-Stationary)')
-    print(f' Significance Level    = {signif}')
-    print(f' Test Statistic        = {result["test_statistic"]}')
-    print(f' No. Lags Chosen       = {result["n_lags"]}')
+    print(f' Null hypothesis: Data has unit root (non-stationary)')
+    print(f' Significance level    = {signif}')
+    print(f' Test statistic        = {result["test_statistic"]}')
+    print(f' Number of lags chosen       = {result["n_lags"]}')
 
     for item,value in t[4].items():
-        print(f' Critical value {adjust(item)} = {round(value, 3)}')
+        print(f' Critical value {aux(item)} = {round(value, 3)}')
 
     if p_value <= signif:
         print(f" => P-Value = {p_value}. Null hypothesis rejected (stationary series).")
